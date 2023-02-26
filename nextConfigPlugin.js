d46e456dac02f309ceb67d9e38141820d7deb155
Hanzel Rodriguez Arias <hayaroar@hotmail.com>
2023-02-26 16:49:58 -0600

    remove custom module rules

--- nextConfigPlugin.js
+++ nextConfigPlugin.js
@@ -1,8 +1,8 @@
-const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
-const path = require('path');
-const loadPayloadConfig = require('./loadPayloadConfig');
-const mockModulePath = path.resolve(__dirname, './mocks/emptyModule.js');
-const customCSSMockPath = path.resolve(__dirname, './mocks/custom.css'); 
+const FilterWarningsPlugin = require("webpack-filter-warnings-plugin");
+const path = require("path");
+const loadPayloadConfig = require("./loadPayloadConfig");
+const mockModulePath = path.resolve(__dirname, "./mocks/emptyModule.js");
+const customCSSMockPath = path.resolve(__dirname, "./mocks/custom.css");
 
 const withPayload = async (config, paths) => {
   const { configPath, cssPath, payloadPath } = paths;
@@ -16,50 +16,54 @@ const withPayload = async (config, paths) => {
       appDir: true,
     },
     webpack: (webpackConfig, webpackOptions) => {
-      const incomingWebpackConfig = typeof config.webpack === 'function' ? config.webpack(webpackConfig, webpackOptions) : webpackConfig;
+      const incomingWebpackConfig =
+        typeof config.webpack === "function"
+          ? config.webpack(webpackConfig, webpackOptions)
+          : webpackConfig;
 
-      incomingWebpackConfig.module.rules.push({
-        oneOf: [
-          {
-            test: /\.(?:ico|gif|png|jpg|jpeg|woff(2)?|eot|ttf|otf|svg)$/i,
-            type: 'asset/resource',
-          },
-        ],
-      })
+      // incomingWebpackConfig.module.rules.push({
+      //   oneOf: [
+      //     {
+      //       test: /\.(?:ico|gif|png|jpg|jpeg|woff(2)?|eot|ttf|otf|svg)$/i,
+      //       type: 'asset/resource',
+      //     },
+      //   ],
+      // })
 
       let newWebpackConfig = {
         ...incomingWebpackConfig,
         plugins: [
-          ...incomingWebpackConfig.plugins || [],
+          ...(incomingWebpackConfig.plugins || []),
           new FilterWarningsPlugin({
-            exclude: [/Critical dependency/, /require.extensions/]
-          })
+            exclude: [/Critical dependency/, /require.extensions/],
+          }),
         ],
         resolve: {
           ...incomingWebpackConfig.resolve,
           alias: {
             ...incomingWebpackConfig.resolve.alias,
-            '@payloadcms/next-payload/getPayload': payloadPath || path.resolve(process.cwd(), './payload.ts'),
-            'payload-config': configPath,
+            "@payloadcms/next-payload/getPayload":
+              payloadPath || path.resolve(process.cwd(), "./payload.ts"),
+            "payload-config": configPath,
             payload$: mockModulePath,
-            'payload-user-css': cssPath || customCSSMockPath,
-          }
-        }
-      }
+            "payload-user-css": cssPath || customCSSMockPath,
+          },
+        },
+      };
 
-      if (typeof payloadConfig.admin.webpack === 'function') {
+      if (typeof payloadConfig.admin.webpack === "function") {
         return payloadConfig.admin.webpack(newWebpackConfig);
       }
 
       return newWebpackConfig;
     },
     transpilePackages: [
-      ...config.transpilePackages || [],
-      '@payloadcms/next-payload',
-      'payload',
-      'mongoose'
-    ]
-  }
-}
+      ...(config.transpilePackages || []),
+      "@payloadcms/next-payload",
+      "payload",
+      "mongoose",
+    ],
+  };
+};
 
-module.exports = withPayload
+module.exports = withPayload;
